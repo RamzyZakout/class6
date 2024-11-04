e24c45bdc7b3f9efb55fad086279b3bf8b6119a9
ramzy zakout <ramzy.zakout@gmail.com>
2024-11-04 12:06:10 +0200

    done

---
+++ Assets/MultiplicationTable.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplicationTable : MonoBehaviour
+{
+   
+    void Start()
+    {
+        Multiply(5, 1);  
+    }
+
+    void Multiply(int num1, int num2)
+    {
+        for (int i = num1; i <= 5; i++)  
+        {
+            for (int j = num2; j <= 10; j++) 
+            {
+                Debug.Log(i*j);
+            }
+        }
+    }
+    
+
+   
+    void Update()
+    {
+        
+    }
+}
--- Assets/rrr.cs
+++ Assets/rrr.cs
@@ -9,58 +9,60 @@ public class rrr : MonoBehaviour
     void Start()
     {
            // ex1
-        int counter = 0;
-        while (counter < 20)
-        {
-            int x = Random.Range(1, 21);
-            Debug.Log(x);
-            counter++;
-            if (x == 5)
-            {
-                break;
-                Debug.Log("kabssa");
+    //     int counter = 0;
+    //     while (counter < 20)
+    //     {
+    //         int x = Random.Range(1, 21);
+    //         Debug.Log(x);
+    //         counter++;
+    //         if (x == 5)
+    //         {
+    //             break;
+    //             Debug.Log("kabssa");
+        
 
 
+    //         }
 
-            }
-
-    //ex2
+    // //ex2
     
-        }
-        int counter2=0;
-         while (counter2 < 20)
-        {
-            int v = Random.Range(1, 21);
-            Debug.Log(v); counter2++;
-            if (v == 5)
-            {
-                Debug.Log(v);
-                continue;
-                Debug.Log("kabssa");
-            }
-        }
-        //ex3
-        string[] word = { "cat", "dog", "car", "pizza", "hat", "fish", "tree", "monkey", "ball", "bird" };
-        int count = 0;
-        string jomla = "";
+    //     }
+    //     int counter2=0;
+    //      while (counter2 < 20)
+    //     {
+    //         int v = Random.Range(1, 21);
+    //         Debug.Log(v); counter2++;
+    //         if (v == 5)
+    //         {
+    //             Debug.Log(v);
+    //             continue;
+    //             Debug.Log("kabssa");
+    //         }
+    //     }
+    //     //ex3
+    //     string[] word = { "cat", "dog", "car", "pizza", "hat", "fish", "tree", "monkey", "ball", "bird" };
+    //     int count = 0;
+    //     string jomla = "";
 
         
-        while (count < 7)
-        {
+    //     while (count < 7)
+    //     {
            
-            int randomSave = Random.Range(0, word.Length);
-            string randomWord = word[randomSave ];
+    //         int randomSave = Random.Range(0, word.Length);
+    //         string randomWord = word[randomSave ];
 
             
-            jomla += randomWord + " ";
-            count++;
-        }
+    //         jomla += randomWord + " ";
+    //         count++;
+    //     }
 
-        Debug.Log(jomla);
-        void Update()
-        {
+    //     Debug.Log(jomla);
+    //     void Update()
+    //     {
 
-        }
+    //     }
 
+
+        
     }
 }
